1ae3665b4d64e6a033bf1bb0c8ae37538a8231e9
iuri <theozzybr@gmail.com>
2018-10-19 22:44:13 -0300

    Mudanças emerson

--- assets/js/pedido.js
+++ assets/js/pedido.js
@@ -4,14 +4,14 @@ var mainApp = angular.module("pedidoApp", []);
             $scope.pedido = {               
                
                 lista:[
-                  {nome:'Laranja',preco:5},
-                  {nome:'Cacau',preco:6},
-                  {nome:'merda',preco:7},
-                  {nome:'merda',preco:7},
-                  {nome:'merda',preco:7},
-                  {nome:'merda',preco:7},
-                  {nome:'merda',preco:7},
-                  {nome:'merda',preco:7},  
+                  {nome:'Laranja',preco:5,checked:false},
+                  {nome:'Cacau',preco:6,checked:false},
+                  {nome:'merda',preco:7,checked:false},
+                  {nome:'merda',preco:7,checked:false},
+                  {nome:'merda',preco:7,checked:false},
+                  {nome:'merda',preco:7,checked:false},
+                  {nome:'merda',preco:7,checked:false},
+                  {nome:'merda',preco:7,checked:false},  
                ],
                checkout:[
                 {nome:'Laranja',preco:5}
@@ -20,7 +20,20 @@ var mainApp = angular.module("pedidoApp", []);
             };
 
             $scope.addProduto = (produto) => {
-              $scope.pedido.total += produto.preco;
-              $scope.pedido.checkout.push(produto);
+              angular.forEach(produto, function(item){
+                if (item.checked)
+                {
+                  console.log(item);
+                    $scope.pedido.lista[item.id] = item.checked;
+                }
+              });
+              console.log($scope.pedido.lista);
+
+              //$scope.pedido.total += produto.preco;
+              //$scope.pedido.checkout.push(produto);
+            }
+            $scope.removProduto = (produto) => {
+              $scope.pedido.total -= produto.preco;
+              // $scope.pedido.checkout.push(produto);
             }
          });
