b4258f854491a23b65ddf758ce7a462f74fe2b3c
iuri <theozzybr@gmail.com>
2018-11-08 17:29:57 -0300

    final

--- assets/js/pedido.js
+++ assets/js/pedido.js
@@ -4,28 +4,33 @@ var mainApp = angular.module("pedidoApp", []);
             $scope.pedido = {               
                
                 lista:[
-                  {nome:'Laranja',preco:5,disabled:false},
-                  {nome:'Cacau',preco:6,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},
-                  {nome:'merda',preco:7,disabled:false},  
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                  {nome:'Laranja',preco:5,desab:false},
+                    
                ],
                checkout:[
-                {nome:'Laranja',preco:5}
+                {nome:'Laranja',preco:5, qtd:1}
                ],
-               total:0
+               total:0,
             };
 
             $scope.addProduto = (produto) => {
-
-              $scope.pedido.total += produto.preco;
               $scope.pedido.checkout.push(produto);
-              $scope.pedido.lista.forEach(function(el) {
-                el.disabled = true;
-              });
+            }
+            $scope.updateTotal = (value) => {
+              $scope.pedido.total += value;
+              console.log(value,$scope.pedido.total);
+            }
+            $scope.somaProduto = (produto) => {
+              //$scope.pedido.total += qtd*lista.preco;
+              $scope.pedido.checkout.qtd = $scope.qtd;
+              //$scope.pedido.total += produto.preco;
+              console.log($scope.pedido.checkout.qtd);
             }
             $scope.removProduto = (produto) => {
               $scope.pedido.total -= produto.preco;
